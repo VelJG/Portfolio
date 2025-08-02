8c4e9b7672a7ac4743828063e8721bc24f574bba
Huynh An Khuong <huynhankhuong0511@gmail.com>
2025-08-02 23:10:21 +0700

    UPdate background and content

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
 
 
-const img = [
-    'img/bg/FNVbg.gif',
-    'img/bg/Yakuzabg.gif',
-    'img/bg/Lethal.png',
-    'img/bg/Stalkerbg.gif',
-    'img/bg/Forestbg.gif',
-    'img/bg/LoPbg.gif'];
+const img = ['img/bg/blackshape.gif'];
 let index = 0;
 const background = document.querySelector('.background');
 
-function changeBackground() {
-   
-    background.style.opacity = 0;
+//function changeBackground() {
 
-  
-    setTimeout(() => {
-        background.src = img[index];
-        index = (index + 1) % img.length;
-        background.style.opacity = 1;
-    }, 1000);
-}
-setInterval(changeBackground, 10000);
-changeBackground();
+//    background.style.opacity = 0;
+
+
+//    setTimeout(() => {
+background.src = img[0];
+//        index = (index + 1) % img.length;
+//        background.style.opacity = 1;
+//    }, 1000);
+//}
+//setInterval(changeBackground, 10000);
+//changeBackground();
 
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
